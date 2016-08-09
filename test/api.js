ff6b2ccf26bd7dae737366cbeeabd7a83a11bb5d
David Merrique <david.merrique@gmail.com>
2016-08-09 18:11:32 -0400

    Working on zip

--- test/api.js
+++ test/api.js
@@ -1,15 +1,28 @@
 import path from 'path';
 import test from 'ava';
 import write from '../lib/write';
-// import read from '../lib/read';
+import mkdir from '../lib/mkdir';
+import read from '../lib/read';
+import zip from '../lib/zip';
 
-test('write', async t => {
-  const data = await write(path.join(__dirname, 'fixtures/testFile'));
-  console.log(data);
-  t.true(true);
-});
+// test('write', async t => {
+//   const data = await write(path.join(__dirname, 'fixtures/testFile'));
+//   console.log(data);
+//   t.true(true);
+// });
 
 // test('read', async t => {
-//   const data = await read('cartridges');
+//   const data = await read('cartridges/test');
+//   t.true(data.length > 0);
+// });
+
+test('zip', async t => {
+  await zip('fixtures/testFile');
+  t.true(data.length > 0);
+});
+
+// test('mkdir', async t => {
+//   const data = await mkdir('test');
+//   console.log(data);
 //   t.true(data.indexOf('URL Not Found') < 0);
 // });
