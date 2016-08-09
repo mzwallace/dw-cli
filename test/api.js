c6806739b0f8e18c98e7bd3272c605303a4c2de4
David Merrique <david.merrique@gmail.com>
2016-08-09 17:34:08 -0400

    write. request

--- test/api.js
+++ test/api.js
@@ -1,13 +1,15 @@
+import path from 'path';
 import test from 'ava';
-// import writeFile from '../lib/writeFile';
-import read from '../lib/read';
+import write from '../lib/write';
+// import read from '../lib/read';
 
-// test('writeFile', async t => {
-// 	await writeFile('fixtures/testFile');
-// 	t.true(true);
-// });
-
-test('read', async t => {
-  const data = await read('cartridges');
-  t.true(data.indexOf('URL Not Found') < 0);
+test('write', async t => {
+  const data = await write(path.join(__dirname, 'fixtures/testFile'));
+  console.log(data);
+  t.true(true);
 });
+
+// test('read', async t => {
+//   const data = await read('cartridges');
+//   t.true(data.indexOf('URL Not Found') < 0);
+// });
