e77fe89f824322861937d0cb07327f6986c42ad4
David Merrique <david.merrique@gmail.com>
2016-08-10 14:31:59 -0400

    Config file

--- commands/push.js
+++ commands/push.js
@@ -4,6 +4,7 @@ const mkdirp = require('mkdirp');
 const zip = require('../lib/zip');
 const unzip = require('../lib/unzip');
 const write = require('../lib/write');
+const mkdir = require('../lib/mkdir');
 
 module.exports = opts => {
   const options = Object.assign({
@@ -17,17 +18,24 @@ module.exports = opts => {
     root: src
   })
   .then(file => {
+    return mkdir({
+      dir: `/${options.env}`
+    })
+    .then(() => file)
+    .catch(() => file);
+  })
+  .then(file => {
     debug(`Zipped ${src} to ${file}`);
-    return write(Object.assign({
+    return write({
       src: file,
       dest: `/${options.env}`
-    }, options));
+    });
   })
   .then(dest => {
     debug(`Uploaded Zip ${dest}`);
-    return unzip(Object.assign({
+    return unzip({
       filePath: dest
-    }, options));
+    });
   })
   .catch(err => {
     console.log(err);
