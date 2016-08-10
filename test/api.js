5f8a85f580996c772c419a64a8e37282bcdc8efb
David Merrique <david.merrique@gmail.com>
2016-08-10 11:48:05 -0400

    Zip file and folder

--- test/api.js
+++ test/api.js
@@ -1,9 +1,16 @@
+import fs from 'fs';
 import path from 'path';
 import test from 'ava';
 import write from '../lib/write';
 import mkdir from '../lib/mkdir';
 import read from '../lib/read';
 import zip from '../lib/zip';
+
+test.after.always('cleanup', () => {
+  // cleanup zip test
+  fs.unlinkSync(path.join(__dirname, 'fixtures/archive.zip'));
+  fs.unlinkSync(path.join(__dirname, 'fixtures/nested.zip'));
+});
 
 // test('write', async t => {
 //   const data = await write(path.join(__dirname, 'fixtures/testFile'));
@@ -16,9 +23,27 @@ import zip from '../lib/zip';
 //   t.true(data.length > 0);
 // });
 
-test('zip', async t => {
-  await zip('fixtures/testFile');
-  t.true(data.length > 0);
+test('zip file', async t => {
+  await zip({
+    src: './fixtures/testFile',
+    dest: './fixtures',
+    name: 'archive.zip'
+  });
+  t.notThrows(() => {
+    fs.accessSync(path.join(__dirname, 'fixtures/archive.zip'));
+  });
+});
+
+test('zip folder', async t => {
+  await zip({
+    src: './fixtures/nested',
+    dest: './fixtures',
+    name: 'nested.zip',
+    root: './fixtures'
+  });
+  t.notThrows(() => {
+    fs.accessSync(path.join(__dirname, 'fixtures/nested.zip'));
+  });
 });
 
 // test('mkdir', async t => {
