01bec2cff515672ccc0bd30f71815610dcacf6a5
Johnny Green <johnnygreen@gmail.com>
2016-08-10 19:35:34 -0400

    switching to new branch util

--- commands/push.js
+++ commands/push.js
@@ -1,19 +1,17 @@
 const path = require('path');
 const chalk = require('chalk');
 const ora = require('ora');
-const git = require('git-rev-sync');
 const get = require('lodash.get');
 const debug = require('debug')('push');
 const zip = require('../lib/zip');
 const unzip = require('../lib/unzip');
 const write = require('../lib/write');
 const mkdir = require('../lib/mkdir');
-
-const branch = git.branch();
+const branch = require('../lib/branch');
 
 module.exports = () => {
   const src = 'cartridges';
-  const spinner = ora(`Deploying ${src}`).start();
+  const spinner = ora(`Deploying ${src} to ${branch()}`).start();
   zip({
     src,
     dest: path.join(get(process, 'env.TMPDIR', '.'), 'archive.zip'),
@@ -21,7 +19,7 @@ module.exports = () => {
   })
   .then(file => {
     return mkdir({
-      dir: `/${branch}`
+      dir: `/${branch()}`
     })
     .then(() => file)
     .catch(() => file);
@@ -30,7 +28,7 @@ module.exports = () => {
     debug(`Zipped ${src} to ${file}`);
     return write({
       src: file,
-      dest: `/${branch}`
+      dest: `/${branch()}`
     });
   })
   .then(dest => {
--- commands/watch.js
+++ commands/watch.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const chalk = require('chalk');
 const ora = require('ora');
-const git = require('git-rev-sync');
 const chokidar = require('chokidar');
 const write = require('../lib/write');
 const mkdirp = require('../lib/mkdirp');
+const branch = require('../lib/branch');
 
 module.exports = () => {
   const watcher = chokidar.watch('dir', {
@@ -21,7 +21,7 @@ module.exports = () => {
   watcher.on('change', filePath => {
     const src = path.relative(process.cwd(), filePath);
     const dir = path.dirname(src).replace('cartridges', '');
-    const dest = path.join('/', git.branch(), dir);
+    const dest = path.join('/', branch(), dir);
     const spinner = ora(`${src} changed, uploading`).start();
     mkdirp(dest).then(() => {
       return write({
