ab07796d747a02f3ccd823a446495940c03d449b
David Merrique <david.merrique@gmail.com>
2016-08-10 12:45:31 -0400

    Working on push

--- commands/push.js
+++ commands/push.js
@@ -1,4 +1,26 @@
-module.exports = options => {
-  console.log(options);
-  console.log('yolo');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const zip = require('../lib/zip');
+const write = require('../lib/write');
+
+module.exports = opts => {
+  const options = Object.assign({
+
+  }, opts);
+  const src = path.join(process.cwd(), 'cartridges');
+  mkdirp.sync(path.join(process.cwd(), '.tmp'));
+  zip({
+    src,
+    dest: path.join(process.cwd(), '.tmp')
+  })
+  .then(file => {
+    return write(file, options);
+  })
+  .then(dest => {
+    console.log(dest);
+  })
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
 };
