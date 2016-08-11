32ac9fdbdf14d8dfdbd04bc7de34c739d431f0e8
David Merrique <david.merrique@gmail.com>
2016-08-11 11:10:57 -0400

    Init test

--- commands/init.js
+++ commands/init.js
@@ -13,13 +13,13 @@ const template = `
 `;
 
 module.exports = () => {
-  fs.statAsync(path.join(process.cwd(), 'dw.json'))
+  return fs.statAsync(path.join(process.cwd(), 'dw.json'))
     .then(() => {
-      process.stdout.write('dw.json already exists');
+      process.stdout.write(chalk.red(`${chalk.underline('dw.json')} already exists`));
     })
     .catch(() => {
       fs.writeFileAsync('dw.json', template.trim(), 'utf8').then(() => {
-        process.stdout.write(chalk.green('dw.json created'));
+        process.stdout.write(chalk.green(`${chalk.underline('dw.json')} created`));
       });
     });
 };
--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,29 @@
+import fs from 'fs';
+import path from 'path';
 import test from 'ava';
 import execa from 'execa';
 
+test.after.always('cleanup', () => {
+  try {
+    fs.unlinkSync(path.join(__dirname, 'fixtures/dw.json'));
+  } catch (e) {
+
+  }
+});
+
 test('push', async t => {
   const result = await execa('../lib/cli.js', ['push', 'dev01', '--folder=fixtures', '--branch=dev']);
-  t.pass(result.stdout === 'Success');
+  t.true(result.stdout === 'Success');
+});
+
+test.serial('init', async t => {
+  process.chdir(path.join(__dirname, 'fixtures'));
+  const result = await execa('../../lib/cli.js', ['init']);
+  t.true(result.stdout === 'dw.json created');
+  process.chdir(__dirname);
+});
+
+test('init exists', async t => {
+  const result = await execa('../lib/cli.js', ['init']);
+  t.true(result.stdout === 'dw.json already exists');
 });
