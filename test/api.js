1d732932fc5467aea85dbbf3bb0021844364864f
David Merrique <david.merrique@gmail.com>
2016-08-11 10:49:25 -0400

    Test tweaks

--- test/api.js
+++ test/api.js
@@ -1,46 +1,55 @@
-// import fs from 'fs';
-// import path from 'path';
+import fs from 'fs';
+import path from 'path';
 import test from 'ava';
-// import write from '../lib/write';
+import write from '../lib/write';
 // import mkdir from '../lib/mkdir';
 // import mkdirp from '../lib/mkdirp';
 // import read from '../lib/read';
-// import zip from '../lib/zip';
+import zip from '../lib/zip';
 // import unzip from '../lib/unzip';
 
-// test.after.always('cleanup', () => {
-//   // cleanup zip test
-//   fs.unlinkSync(path.join(__dirname, 'fixtures/archive.zip'));
-//   fs.unlinkSync(path.join(__dirname, 'fixtures/nested.zip'));
-// });
+test.after.always('cleanup', () => {
+  // cleanup zip test
+  try {
+    fs.unlinkSync(path.join(__dirname, 'fixtures/archive.zip'));
+    fs.unlinkSync(path.join(__dirname, 'fixtures/nested.zip'));
+  } catch (e) {
 
-test.todo('write');
+  }
+});
+
+test('write', async t => {
+  const data = await write({
+    src: 'fixtures/testFile'
+  });
+  t.true(data === '/testFile');
+});
 
 // test('read', async t => {
 //   const data = await read('cartridges/test');
 //   t.true(data.length > 0);
 // });
 
-// test('zip file', async t => {
-//   await zip({
-//     src: 'fixtures/testFile',
-//     dest: 'fixtures/archive.zip'
-//   });
-//   t.notThrows(() => {
-//     fs.accessSync(path.join(__dirname, 'fixtures/archive.zip'));
-//   });
-// });
-//
-// test('zip folder', async t => {
-//   await zip({
-//     src: 'fixtures/nested',
-//     dest: 'fixtures/nested.zip',
-//     root: 'fixtures'
-//   });
-//   t.notThrows(() => {
-//     fs.accessSync(path.join(__dirname, 'fixtures/nested.zip'));
-//   });
-// });
+test('zip file', async t => {
+  await zip({
+    src: 'fixtures/testFile',
+    dest: 'fixtures/archive.zip'
+  });
+  t.notThrows(() => {
+    fs.accessSync(path.join(__dirname, 'fixtures/archive.zip'));
+  });
+});
+
+test('zip folder', async t => {
+  await zip({
+    src: 'fixtures/nested',
+    dest: 'fixtures/nested.zip',
+    root: 'fixtures'
+  });
+  t.notThrows(() => {
+    fs.accessSync(path.join(__dirname, 'fixtures/nested.zip'));
+  });
+});
 
 // test('mkdir', async t => {
 //   const data = await mkdir('test');
