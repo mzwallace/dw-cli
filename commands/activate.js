3c7de960df4f8a4801f4cf8a7c19b90371b28ecf
David Merrique <david.merrique@gmail.com>
2016-08-30 10:42:21 -0400

    Activate

--- commands/activate.js
+++ commands/activate.js
@@ -1,8 +1,98 @@
 const ora = require('ora');
-const get = require('lodash.get');
+const chalk = require('chalk');
+const request = require('request');
+const config = require('../lib/config')();
 
-module.exports = () => {
-  const branch = global.argv.branch;
+const authenticate = () => {
+  let req;
+  const promise = new Promise((resolve, reject) => {
+    req = request.post('https://account.demandware.com/dw/oauth2/access_token', {
+      auth: {
+        username: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        password: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+      },
+      form: {
+        grant_type: 'client_credentials' // eslint-disable-line
+      }
+    }, (err, res, body) => {
+      if (err) {
+        return reject(err);
+      }
+      if (res.statusCode >= 400) {
+        return reject(new Error(res.statusMessage));
+      }
+      resolve(body);
+    });
+  });
+  promise.request = req;
+  return promise;
+};
 
+// const getVersions = token => {
+//   let req;
+//   const promise = new Promise((resolve, reject) => {
+//     req = request.get('https://dev01-us-mzw.demandware.net/s/-/dw/data/v16_6/code_versions', {
+//       auth: {
+//         bearer: token
+//       }
+//     }, (err, res, body) => {
+//       if (err) {
+//         return reject(err);
+//       }
+//       if (res.statusCode >= 400) {
+//         return reject(new Error(res.body.fault.message));
+//       }
+//       resolve(JSON.parse(body));
+//     });
+//   });
+//   promise.request = req;
+//   return promise;
+// };
 
+const activateVersion = ({token, env, codeversion}) => {
+  let req;
+  const promise = new Promise((resolve, reject) => {
+    req = request.patch(`https://${env}${config.hostname}/s/-/dw/data/v16_6/code_versions/${codeversion}`, {
+      auth: {
+        bearer: token
+      },
+      json: true,
+      body: {
+        active: true
+      }
+    }, (err, res, body) => {
+      if (err) {
+        return reject(err);
+      }
+      if (res.statusCode >= 400) {
+        return reject(res.body.fault.message);
+      }
+      resolve(body);
+    });
+  });
+  promise.request = req;
+  return promise;
+};
+
+module.exports = function ({env, codeversion}) {
+  const spinner = ora(`Deploying ${codeversion} on ${env}`).start();
+
+  authenticate()
+    .then(resp => {
+      return Promise.resolve(JSON.parse(resp).access_token);
+    })
+    .then(token => {
+      return activateVersion({
+        env,
+        token,
+        codeversion
+      }).then(() => {
+        spinner.succeed();
+        process.stdout.write(chalk.green('Success'));
+      });
+    })
+    .catch(err => {
+      spinner.fail();
+      process.stdout.write(chalk.red(err));
+    });
 };
