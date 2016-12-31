788f77c1d8d4d733f7318f84307717a16ca514b6
David Merrique <david.merrique@gmail.com>
2016-12-31 15:17:27 -0500

    Fix init

--- commands/init.js
+++ commands/init.js
@@ -5,13 +5,12 @@ const log = require('../lib/log');
 
 const fsAsync = Promise.promisifyAll(fs);
 
-const template = fs.readFileSync(path.join(__dirname, '../dw.json.example'));
-
 module.exports = async () => {
   try {
     await fsAsync.statAsync(path.join(process.cwd(), 'dw.json'));
     log.error(`'dw.json' already exists`);
   } catch (err) {
+    const template = await fsAsync.readFileAsync(path.join(__dirname, '../dw.json.example'), 'utf8');
     await fsAsync.writeFileAsync('dw.json', template.trim(), 'utf8');
     log.success(`'dw.json' created`);
   }
