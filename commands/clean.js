217f9cac5bbc2796c9567769107bf797352b5291
David Merrique <david.merrique@gmail.com>
2017-01-10 18:10:44 -0500

    Already clean message

--- commands/clean.js
+++ commands/clean.js
@@ -5,7 +5,7 @@ const log = require('../lib/log');
 const api = require('../lib/api');
 
 module.exports = async ({clientId, clientPassword, hostname, apiVersion, webdav, request}) => {
-  log.info(`Removing all inactive code versions from ${webdav}`);
+  log.info(`Cleaning up ${webdav}`);
   const spinner = ora();
 
   try {
@@ -13,24 +13,26 @@ module.exports = async ({clientId, clientPassword, hostname, apiVersion, webdav,
     const method = 'get';
     const endpoint = `https://${hostname}/s/-/dw/data/${apiVersion}/code_versions`;
     const {data} = await api({clientId, clientPassword, method, endpoint});
-    spinner.succeed();
     if (data.length === 1) {
-      return log.info(`${data[0].id} is the only version`);
+      spinner.text = `Already clean`;
+      spinner.succeed();
+    } else {
+      spinner.succeed();
+      log.plain('-------------------');
+      spinner.text = 'Removing';
+      spinner.start();
+      await Promise.map(data, async version => {
+        if (!version.active) {
+          await del(`/Cartridges/${version.id}`, request);
+          spinner.text = `Removed ${version.id}`;
+          spinner.succeed();
+          spinner.text = 'Removing';
+          spinner.start();
+        }
+      });
+      spinner.stop();
+      log.plain('-------------------');
     }
-    log.plain('-------------------');
-    spinner.text = 'Removing';
-    spinner.start();
-    await Promise.map(data, async version => {
-      if (!version.active) {
-        await del(`/Cartridges/${version.id}`, request);
-        spinner.text = `Removed ${version.id}`;
-        spinner.succeed();
-        spinner.text = 'Removing';
-        spinner.start();
-      }
-    });
-    spinner.stop();
-    log.plain('-------------------');
     log.success('Success');
   } catch (err) {
     spinner.fail();
