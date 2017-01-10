ebe2e036a2897fbc0a7fc7b397a37fcf7707f3a1
Johnny Green <johnnygreen@gmail.com>
2017-01-10 17:30:17 -0500

    adds remove and clean commands

---
+++ commands/clean.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const ora = require('ora');
+const Promise = require('bluebird');
+const del = require('../lib/delete');
+const log = require('../lib/log');
+const api = require('../lib/api');
+
+module.exports = async ({clientId, clientPassword, hostname, apiVersion, webdav, request}) => {
+  log.info(`Removing all inactive code versions from ${webdav}`);
+  const spinner = ora();
+
+  try {
+    let file;
+
+    spinner.text = 'Reading';
+    const method = 'get';
+    const endpoint = `https://${hostname}/s/-/dw/data/${apiVersion}/code_versions`;
+    const {data} = await api({clientId, clientPassword, method, endpoint});
+    spinner.succeed();
+
+    spinner.text = 'Removing';
+    spinner.start();
+    Promise.map(data, async version => {
+      if ( ! version.active) {
+        await del(`/Cartridges/${version.id}`, request);
+        spinner.text = `Removed ${version.id}`;
+        spinner.succeed();
+        spinner.text = 'Removing';
+        spinner.start();
+      }
+    }).then(() => {
+      spinner.stop();
+      log.success('Success');
+    });
+  } catch (err) {
+    spinner.fail();
+    log.error(err);
+  }
+};
---
+++ commands/remove.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const ora = require('ora');
+const del = require('../lib/delete');
+const log = require('../lib/log');
+
+module.exports = async ({codeVersion, webdav, request}) => {
+  log.info(`Removing ${codeVersion} from ${webdav}`);
+  const spinner = ora();
+
+  try {
+    let file;
+
+    spinner.start();
+    spinner.text = `Removing`;
+    await del(`/Cartridges/${codeVersion}`, request);
+    spinner.succeed();
+    log.success('Success');
+  } catch (err) {
+    spinner.fail();
+    log.error(err);
+  }
+};
